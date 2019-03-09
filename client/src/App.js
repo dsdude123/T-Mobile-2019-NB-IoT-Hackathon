2c1156257efc5efff07fe5ef30b11905ad66e0f2
rckeller <rykeller@uw.edu>
2019-03-08 23:04:42 -0800

    IT WORKS

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 
-import L from 'leaflet'; // eslint-disable-line
-// postCSS import of Leaflet's CSS
-import 'leaflet/dist/leaflet.css';
-// using webpack json loader we can import our geojson file like this
-// import geojson from 'json!./bk_subway_entrances.geojson';
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
 
 const position = [51.505, -0.09]
 
@@ -31,15 +27,27 @@ class App extends Component {
         </header> */}
         <article>
           <section>
-            <Map center={position} zoom={13}>
+            <LeafletMap
+              center={[50, 10]}
+              zoom={6}
+              maxZoom={10}
+              attributionControl={true}
+              zoomControl={true}
+              doubleClickZoom={true}
+              scrollWheelZoom={true}
+              dragging={true}
+              animate={true}
+              easeLinearity={0.35}
+            >
               <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
               />
-              <Marker position={position}>
-                <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+              <Marker position={[50, 10]}>
+                <Popup>
+                  Popup for any custom information.
+                </Popup>
               </Marker>
-            </Map>
+            </LeafletMap>
           </section>
         </article>
       </div>
