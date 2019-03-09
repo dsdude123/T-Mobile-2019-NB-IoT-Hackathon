41a8031407a89ad3b34bcc19960d4ccfc878601b
rckeller <rykeller@uw.edu>
2019-03-09 11:03:41 -0800

    lorem ipsum UI

--- client/src/containers/ReactMD.js
+++ client/src/containers/ReactMD.js
@@ -1,22 +1,62 @@
 import React from 'react'
 
-import { NavigationDrawer, FontIcon } from 'react-md'
+import { NavigationDrawer, FontIcon, ListItem, Subheader, Divider } from 'react-md'
 
 export default class ReactMD extends React.Component {
-  navItems = []
+  navItems = [
+    <ListItem
+      key='health'
+      primaryText='Overall Health'
+      secondaryText='10/50'
+      rightIcon={<FontIcon>timeline</FontIcon>}
+    />,
+    <ListItem
+      key='last_ouatage'
+      primaryText='Last Outage'
+      secondaryText='04/09 at 10:10AM PST'
+      rightIcon={<FontIcon>av_timer</FontIcon>}
+    />,
+    <Divider/>,
+    <Subheader>Developer Resources</Subheader>,
+    <a
+      key='source'
+      target='_blank'
+      rel='noopener noreferrer'
+      href='https://github.com/dsdude123/T-Mobile-2019-NB-IoT-Hackathon/'
+    >
+      <ListItem
+        primaryText='Source Code'
+        secondaryText='On GitHub'
+        rightIcon={<FontIcon>code</FontIcon>}
+        />
+    </a>,
+    <a
+      key='docs'
+      target='_blank'
+      rel='noopener noreferrer'
+      href='https://github.com/dsdude123/T-Mobile-2019-NB-IoT-Hackathon/blob/cra-v2/API.md'
+    >
+      <ListItem
+        primaryText='IoT Device API'
+        secondaryText='Official Docs'
+        rightIcon={<FontIcon>link</FontIcon>}
+      />
+    </a>
+  ]
   render () {
     return (
       <NavigationDrawer
-          contentId='app'
-          toolbarTitle='T-Mobile IOT Hack'
-          drawerTitle='Tracker'
-          mobileDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
-          tabletDrawerType={NavigationDrawer.DrawerTypes.FLOATING}
-          desktopDrawerType={NavigationDrawer.DrawerTypes.FLOATING}
-          navItems={this.navItems}
-        >
-          {this.props.children}
-        </NavigationDrawer>
+        contentId='app'
+        toolbarTitle='T-Mobile IOT Hack - Device Monitor'
+        drawerTitle='Status Report'
+        mobileDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
+        tabletDrawerType={NavigationDrawer.DrawerTypes.FLOATING}
+        desktopDrawerType={NavigationDrawer.DrawerTypes.FLOATING}
+        desktopMinWidth={400}
+        navItems={this.navItems}
+      >
+        {this.props.children}
+      </NavigationDrawer>
     )
   }
 }
