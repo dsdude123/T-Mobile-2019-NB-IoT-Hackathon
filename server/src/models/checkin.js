3b02d5c9621726c1fd7e828aba13859778983443
rckeller <rykeller@uw.edu>
2019-03-09 10:31:22 -0800

    Puget sound INTs, floats not avail yet

--- server/src/models/checkin.js
+++ server/src/models/checkin.js
@@ -25,8 +25,17 @@ module.exports.seed = (ids, i) => {
     _id: ids['Checkin'][i],
     device: ids['Device'][i],
     timestamp: faker.date.recent(14),
-    latitude: faker.random.number({ min: -90, max: 90 }),
-    longitude: faker.random.number({ min: -180, max: 180 }),
+
+    /*
+    Example coordinate range based on Puget Sound
+    https://www.latlong.net/
+    48.101399, -122.889235
+    47.080475, -121.596835
+    */
+    latitude: faker.random.number({ min: 47.080475, max: 48.101399 }),
+    longitude: faker.random.number({ min: -122.889235, max: -121.596835 }),
+    // latitude: faker.random.number({ min: -90, max: 90 }),
+    // longitude: faker.random.number({ min: -180, max: 180 }),
     elevation: faker.random.boolean ? faker.random.number({ min: -1000, max: 6000 }) : null,
     status: faker.random.arrayElement(['OK', 'FAIL']),
     temperature: faker.random.boolean ? faker.random.number({ min: -20, max: 80 }) : null,
