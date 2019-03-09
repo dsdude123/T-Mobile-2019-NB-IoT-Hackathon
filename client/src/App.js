2d7213a7d476e62d9ce177f8322ca85d9a1e6ad9
rckeller <rykeller@uw.edu>
2019-03-08 23:02:27 -0800

    Ugh, errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import L from 'leaflet'; // eslint-disable-line
+// postCSS import of Leaflet's CSS
+import 'leaflet/dist/leaflet.css';
+// using webpack json loader we can import our geojson file like this
+// import geojson from 'json!./bk_subway_entrances.geojson';
+
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [51.505, -0.09]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +28,20 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <article>
+          <section>
+            <Map center={position} zoom={13}>
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              />
+              <Marker position={position}>
+                <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+              </Marker>
+            </Map>
+          </section>
+        </article>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
+import './load-leaflet'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
